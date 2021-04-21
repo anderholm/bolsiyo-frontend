c3819e2aaaf040136c691045df850687d7545b2b
Anderson <anderholmquist@gmail.com>
2021-04-20 23:30:46 -0400

    borrando console.log

--- src/app/component/image-detail/image-detail.component.ts
+++ src/app/component/image-detail/image-detail.component.ts
@@ -25,7 +25,6 @@ export class ImageDetailComponent implements OnInit {
   cargarImagenDetallada(id:string) {
     this.cardService.getCardInfoById(id).subscribe((response) => {
       this.response = response;
-      console.log(response)
     });
   }
 }
